46be408a9087438a9d7578cb3fb8b4cb92820225
surya16ajah <suryabayu945@gmail.com>
2026-08-06 09:29:24 +0700

    Method DELETE & Truncate Table

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -59,8 +59,12 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Product deleted successfully',
+        ], Response::HTTP_OK);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/products', [ProductController::class, 'index'])->name('product');
-
 Route::post('/products', [ProductController::class, 'store'])->name('product.store');
-
 Route::put('/products/{product}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
+
